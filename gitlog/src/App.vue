ff04795e26525518fd362b4892cc2b2e3b67b58d
Farewelliu <1600653272@qq.com>
2019-03-26 23:54:37 +0800

    how to created a vue project

--- gitlog/src/App.vue
+++ gitlog/src/App.vue
@@ -1,23 +1,41 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+ <div style="height: 500px;">
+   <helloWorld></helloWorld> 
+  <el-steps direction="vertical" :active="1">
+    <el-step title="步骤 1" description="安装node,npm,vue"></el-step>
+    <el-step title="步骤 2" description="cnpm install -g vue-cli"></el-step>
+    <el-step title="步骤 3" description="cnpm install -g webpack"></el-step>
+    <el-step title="步骤 4" description="vue init webpack <fileName>"></el-step>
+    <el-step title="步骤 5" description="cd <fileName>"></el-step>
+    <el-step title="步骤 6" description="npm install"></el-step>
+    <el-step title="步骤 7" description="npm run dev"></el-step>
+    <el-step title="步骤 8" description="网页输入localhost:端口号"></el-step>
+  </el-steps>
+</div>
 </template>
-
 <script>
-export default {
-  name: 'App'
-}
+import helloWorld from './components/Helloworld'
+  export default {
+    data() {
+      return{
+        list:[{
+          'text':"安装node,npm,vue",
+          'text':"cnpm install -g webpack",
+          'text':"cnpm install -g vue-cli",
+          'text':"vue init webpack <fileName>",
+          'text':"cd <fileName>",
+          'text':"npm install",
+          'text':"npm run dev",
+          'text':"网页输入localhost:端口号",
+        }]
+      }
+      
+    },
+    components: {
+      helloWorld
+    }
+  };
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ 
 </style>
--- gitlog/src/components/HelloWorld.vue
+++ gitlog/src/components/HelloWorld.vue
@@ -1,113 +1,25 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+    <p v-html='msg'></p>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    data(){
+        return{
+            msg:"welcome to my vueCenter~<br><strong>Let's Do it!</strong>"
+        }
     }
-  }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<style>
+p{
+    padding: 10px 20px;
+    background-color: #3399ff;
+    color: #fff;  
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+strong{
+    font-size: 20px;
+    color:#000;
 }
 </style>
+
+
