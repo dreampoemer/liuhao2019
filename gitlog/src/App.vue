278d838fd5761ce20d174d1ecbd2687acd0d252c
Farewelliu <1600653272@qq.com>
2019-05-31 17:16:57 +0800

    new changes

--- gitlog/src/App.vue
+++ gitlog/src/App.vue
@@ -2,14 +2,15 @@
  <div style="height: 500px;">
    <helloWorld></helloWorld> 
   <el-steps direction="vertical" :active="1">
-    <el-step title="步骤 1" description="安装node,npm,vue"></el-step>
+    <!-- <el-step title="步骤 1" description="安装node,npm,vue"></el-step>
     <el-step title="步骤 2" description="cnpm install -g vue-cli"></el-step>
     <el-step title="步骤 3" description="cnpm install -g webpack"></el-step>
     <el-step title="步骤 4" description="vue init webpack <fileName>"></el-step>
     <el-step title="步骤 5" description="cd <fileName>"></el-step>
     <el-step title="步骤 6" description="npm install"></el-step>
     <el-step title="步骤 7" description="npm run dev"></el-step>
-    <el-step title="步骤 8" description="网页输入localhost:端口号"></el-step>
+    <el-step title="步骤 8" description="网页输入localhost:端口号"></el-step> -->
+    <el-step v-for="item in list" title="步骤" description="{{item.text}}"></el-step>
   </el-steps>
 </div>
 </template>
@@ -26,7 +27,7 @@ import helloWorld from './components/Helloworld'
           'text':"cd <fileName>",
           'text':"npm install",
           'text':"npm run dev",
-          'text':"网页输入localhost:端口号",
+          'text':"网页输入localhost:端口号"
         }]
       }
       
